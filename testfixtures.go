d0211dcba1fb3baf8ef09e30ad3c66400aeeec4e
Andrey Nering <andrey.nering@gmail.com>
2016-07-17 20:26:31 -0300

    Allow the fixture file to be either a slice or a map

--- testfixtures.go
+++ testfixtures.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"path"
 	"path/filepath"
+	"reflect"
 	"strings"
 
 	"gopkg.in/yaml.v2"
@@ -17,6 +18,17 @@ type fixtureFile struct {
 	content  []byte
 }
 
+var (
+	// ErrWrongCastNotAMap is returned when a map is not a map[interface{}]interface{}
+	ErrWrongCastNotAMap = fmt.Errorf("Could not cast record: not a map[interface{}]interface{}")
+
+	// ErrFileIsNotSliceOrMap is returned the the fixture file is not a slice or map.
+	ErrFileIsNotSliceOrMap = fmt.Errorf("The fixture file is not a slice or map")
+
+	// ErrKeyIsNotString is returned when a record is not of type string
+	ErrKeyIsNotString = fmt.Errorf("Record map key is not string")
+)
+
 func (f *fixtureFile) fileNameWithoutExtension() string {
 	return strings.Replace(f.fileName, filepath.Ext(f.fileName), "", 1)
 }
@@ -26,51 +38,91 @@ func (f *fixtureFile) delete(tx *sql.Tx, h DataBaseHelper) error {
 	return err
 }
 
+func (f *fixtureFile) buildInsertSQL(h DataBaseHelper, record map[interface{}]interface{}) (sqlStr string, values []interface{}, err error) {
+	var sqlColumns string
+	var sqlValues string
+	i := 1
+	for key, value := range record {
+		if len(sqlColumns) > 0 {
+			sqlColumns += ", "
+			sqlValues += ", "
+		}
+		keyStr, ok := key.(string)
+		if !ok {
+			err = ErrKeyIsNotString
+			return
+		}
+		sqlColumns += h.quoteKeyword(keyStr)
+		switch h.paramType() {
+		case paramTypeDollar:
+			sqlValues += fmt.Sprintf("$%d", i)
+		case paramTypeQuestion:
+			sqlValues += "?"
+		case paramTypeColon:
+			if isDateTime(value) {
+				sqlValues += fmt.Sprintf("to_date(:%d, 'YYYY-MM-DD HH24:MI:SS')", i)
+			} else if isDate(value) {
+				sqlValues += fmt.Sprintf("to_date(:%d, 'YYYY-MM-DD')", i)
+			} else if isTime(value) {
+				sqlValues += fmt.Sprintf("to_date(:%d, 'HH24:MI:SS')", i)
+			} else {
+				sqlValues += fmt.Sprintf(":%d", i)
+			}
+		}
+		i++
+		values = append(values, value)
+	}
+
+	sqlStr = fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", h.quoteKeyword(f.fileNameWithoutExtension()), sqlColumns, sqlValues)
+	return
+}
+
 func (f *fixtureFile) insert(tx *sql.Tx, h DataBaseHelper) error {
-	var rows []interface{}
+	var rows interface{}
 	err := yaml.Unmarshal(f.content, &rows)
 	if err != nil {
 		return err
 	}
 
-	for _, row := range rows {
-		record := row.(map[interface{}]interface{})
-		var values []interface{}
-
-		sqlColumns := ""
-		sqlValues := ""
-		i := 1
-		for key, value := range record {
-			if len(sqlColumns) > 0 {
-				sqlColumns += ", "
-				sqlValues += ", "
+	t := reflect.TypeOf(rows)
+	v := reflect.ValueOf(rows)
+	switch t.Kind() {
+	case reflect.Slice:
+		length := v.Len()
+		for i := 0; i < length; i++ {
+			record, ok := v.Index(i).Interface().(map[interface{}]interface{})
+			if !ok {
+				return ErrWrongCastNotAMap
 			}
-			sqlColumns += h.quoteKeyword(key.(string))
-			switch h.paramType() {
-			case paramTypeDollar:
-				sqlValues += fmt.Sprintf("$%d", i)
-			case paramTypeQuestion:
-				sqlValues += "?"
-			case paramTypeColon:
-				if isDateTime(value) {
-					sqlValues += fmt.Sprintf("to_date(:%d, 'YYYY-MM-DD HH24:MI:SS')", i)
-				} else if isDate(value) {
-					sqlValues += fmt.Sprintf("to_date(:%d, 'YYYY-MM-DD')", i)
-				} else if isTime(value) {
-					sqlValues += fmt.Sprintf("to_date(:%d, 'HH24:MI:SS')", i)
-				} else {
-					sqlValues += fmt.Sprintf(":%d", i)
-				}
-			}
-			i++
-			values = append(values, value)
-		}
 
-		sql := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", h.quoteKeyword(f.fileNameWithoutExtension()), sqlColumns, sqlValues)
-		_, err = tx.Exec(sql, values...)
-		if err != nil {
-			return err
+			sqlStr, values, err := f.buildInsertSQL(h, record)
+			if err != nil {
+				return err
+			}
+			_, err = tx.Exec(sqlStr, values...)
+			if err != nil {
+				return err
+			}
 		}
+	case reflect.Map:
+		keys := v.MapKeys()
+		for _, key := range keys {
+			record, ok := v.MapIndex(key).Interface().(map[interface{}]interface{})
+			if !ok {
+				return ErrWrongCastNotAMap
+			}
+
+			sqlStr, values, err := f.buildInsertSQL(h, record)
+			if err != nil {
+				return err
+			}
+			_, err = tx.Exec(sqlStr, values...)
+			if err != nil {
+				return err
+			}
+		}
+	default:
+		return ErrFileIsNotSliceOrMap
 	}
 	return nil
 }
