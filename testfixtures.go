d0e68ddfca8a297ff59858878268c9f4b56dbddb
Andrey Nering <andrey.nering@gmail.com>
2016-10-12 17:38:14 -0300

    Caching SQL statements and other small improvements

--- testfixtures.go
+++ testfixtures.go
@@ -6,7 +6,6 @@ import (
 	"io/ioutil"
 	"path"
 	"path/filepath"
-	"reflect"
 	"strings"
 
 	"gopkg.in/yaml.v2"
@@ -18,62 +17,16 @@ type Context struct {
 	fixturesFiles []*fixtureFile
 }
 
-func NewFolder(db *sql.DB, helper Helper, folderName string) (*Context, error) {
-	fixtures, err := fixturesFromFolder(folderName)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Context{
-		db:            db,
-		helper:        helper,
-		fixturesFiles: fixtures,
-	}, nil
-}
-
-func NewFiles(db *sql.DB, helper Helper, fileNames ...string) (*Context, error) {
-	fixtures, err := fixturesFromFiles(fileNames...)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Context{
-		db:            db,
-		helper:        helper,
-		fixturesFiles: fixtures,
-	}, nil
-}
-
-func (c *Context) Load() error {
-	if !skipDatabaseNameCheck {
-		if !dbnameRegexp.MatchString(c.helper.databaseName(c.db)) {
-			return errNotTestDatabase
-		}
-	}
-
-	err := c.helper.disableReferentialIntegrity(c.db, func(tx *sql.Tx) error {
-		for _, file := range c.fixturesFiles {
-			err := file.delete(tx, c.helper)
-			if err != nil {
-				return err
-			}
-
-			err = c.helper.whileInsertOnTable(tx, file.fileNameWithoutExtension(), func() error {
-				return file.insert(tx, c.helper)
-			})
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-	return err
-}
-
 type fixtureFile struct {
-	path     string
-	fileName string
-	content  []byte
+	path       string
+	fileName   string
+	content    []byte
+	insertSQLs []insertSQL
+}
+
+type insertSQL struct {
+	sql    string
+	params []interface{}
 }
 
 var (
@@ -87,6 +40,118 @@ var (
 	ErrKeyIsNotString = fmt.Errorf("Record map key is not string")
 )
 
+func NewFolder(db *sql.DB, helper Helper, folderName string) (*Context, error) {
+	fixtures, err := fixturesFromFolder(folderName)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Context{
+		db:            db,
+		helper:        helper,
+		fixturesFiles: fixtures,
+	}
+
+	if err := c.buildInsertSQLs(); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func NewFiles(db *sql.DB, helper Helper, fileNames ...string) (*Context, error) {
+	fixtures, err := fixturesFromFiles(fileNames...)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &Context{
+		db:            db,
+		helper:        helper,
+		fixturesFiles: fixtures,
+	}
+
+	if err := c.buildInsertSQLs(); err != nil {
+		return nil, err
+	}
+
+	return c, nil
+}
+
+func (c *Context) Load() error {
+	if !skipDatabaseNameCheck {
+		if !dbnameRegexp.MatchString(c.helper.databaseName(c.db)) {
+			return errNotTestDatabase
+		}
+	}
+
+	err := c.helper.disableReferentialIntegrity(c.db, func(tx *sql.Tx) error {
+		for _, file := range c.fixturesFiles {
+			if err := file.delete(tx, c.helper); err != nil {
+				return err
+			}
+
+			err := c.helper.whileInsertOnTable(tx, file.fileNameWithoutExtension(), func() error {
+				for _, i := range file.insertSQLs {
+					if _, err := tx.Exec(i.sql, i.params...); err != nil {
+						return err
+					}
+				}
+				return nil
+			})
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	return err
+}
+
+func (c *Context) buildInsertSQLs() error {
+	for _, f := range c.fixturesFiles {
+		var records interface{}
+		if err := yaml.Unmarshal(f.content, &records); err != nil {
+			return err
+		}
+
+		switch records := records.(type) {
+		case []interface{}:
+			for _, record := range records {
+				recordMap, ok := record.(map[interface{}]interface{})
+				if !ok {
+					return ErrWrongCastNotAMap
+				}
+
+				sql, values, err := f.buildInsertSQL(c.helper, recordMap)
+				if err != nil {
+					return err
+				}
+
+				f.insertSQLs = append(f.insertSQLs, insertSQL{sql, values})
+			}
+		case map[interface{}]interface{}:
+			for _, record := range records {
+				recordMap, ok := record.(map[interface{}]interface{})
+				if !ok {
+					return ErrWrongCastNotAMap
+				}
+
+				sql, values, err := f.buildInsertSQL(c.helper, recordMap)
+				if err != nil {
+					return err
+				}
+
+				f.insertSQLs = append(f.insertSQLs, insertSQL{sql, values})
+			}
+		default:
+			return ErrFileIsNotSliceOrMap
+		}
+	}
+
+	return nil
+}
+
 func (f *fixtureFile) fileNameWithoutExtension() string {
 	return strings.Replace(f.fileName, filepath.Ext(f.fileName), "", 1)
 }
@@ -97,92 +162,48 @@ func (f *fixtureFile) delete(tx *sql.Tx, h Helper) error {
 }
 
 func (f *fixtureFile) buildInsertSQL(h Helper, record map[interface{}]interface{}) (sqlStr string, values []interface{}, err error) {
-	var sqlColumns string
-	var sqlValues string
-	i := 1
+	var (
+		sqlColumns []string
+		sqlValues  []string
+		i          = 1
+	)
 	for key, value := range record {
-		if len(sqlColumns) > 0 {
-			sqlColumns += ", "
-			sqlValues += ", "
-		}
 		keyStr, ok := key.(string)
 		if !ok {
 			err = ErrKeyIsNotString
 			return
 		}
-		sqlColumns += h.quoteKeyword(keyStr)
+
+		sqlColumns = append(sqlColumns, h.quoteKeyword(keyStr))
+
 		switch h.paramType() {
 		case paramTypeDollar:
-			sqlValues += fmt.Sprintf("$%d", i)
+			sqlValues = append(sqlValues, fmt.Sprintf("$%d", i))
 		case paramTypeQuestion:
-			sqlValues += "?"
+			sqlValues = append(sqlValues, "?")
 		case paramTypeColon:
-			if isDateTime(value) {
-				sqlValues += fmt.Sprintf("to_date(:%d, 'YYYY-MM-DD HH24:MI:SS')", i)
-			} else if isDate(value) {
-				sqlValues += fmt.Sprintf("to_date(:%d, 'YYYY-MM-DD')", i)
-			} else if isTime(value) {
-				sqlValues += fmt.Sprintf("to_date(:%d, 'HH24:MI:SS')", i)
-			} else {
-				sqlValues += fmt.Sprintf(":%d", i)
+			switch {
+			case isDateTime(value):
+				sqlValues = append(sqlValues, fmt.Sprintf("to_date(:%d, 'YYYY-MM-DD HH24:MI:SS')", i))
+			case isDate(value):
+				sqlValues = append(sqlValues, fmt.Sprintf("to_date(:%d, 'YYYY-MM-DD')", i))
+			case isTime(value):
+				sqlValues = append(sqlValues, fmt.Sprintf("to_date(:%d, 'HH24:MI:SS')", i))
+			default:
+				sqlValues = append(sqlValues, fmt.Sprintf(":%d", i))
 			}
 		}
 		i++
 		values = append(values, value)
 	}
 
-	sqlStr = fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", h.quoteKeyword(f.fileNameWithoutExtension()), sqlColumns, sqlValues)
+	sqlStr = fmt.Sprintf(
+		"INSERT INTO %s (%s) VALUES (%s)",
+		h.quoteKeyword(f.fileNameWithoutExtension()),
+		strings.Join(sqlColumns, ", "),
+		strings.Join(sqlValues, ", "),
+	)
 	return
-}
-
-func (f *fixtureFile) insert(tx *sql.Tx, h Helper) error {
-	var rows interface{}
-	err := yaml.Unmarshal(f.content, &rows)
-	if err != nil {
-		return err
-	}
-
-	t := reflect.TypeOf(rows)
-	v := reflect.ValueOf(rows)
-	switch t.Kind() {
-	case reflect.Slice:
-		length := v.Len()
-		for i := 0; i < length; i++ {
-			record, ok := v.Index(i).Interface().(map[interface{}]interface{})
-			if !ok {
-				return ErrWrongCastNotAMap
-			}
-
-			sqlStr, values, err := f.buildInsertSQL(h, record)
-			if err != nil {
-				return err
-			}
-			_, err = tx.Exec(sqlStr, values...)
-			if err != nil {
-				return err
-			}
-		}
-	case reflect.Map:
-		keys := v.MapKeys()
-		for _, key := range keys {
-			record, ok := v.MapIndex(key).Interface().(map[interface{}]interface{})
-			if !ok {
-				return ErrWrongCastNotAMap
-			}
-
-			sqlStr, values, err := f.buildInsertSQL(h, record)
-			if err != nil {
-				return err
-			}
-			_, err = tx.Exec(sqlStr, values...)
-			if err != nil {
-				return err
-			}
-		}
-	default:
-		return ErrFileIsNotSliceOrMap
-	}
-	return nil
 }
 
 func fixturesFromFolder(folderName string) ([]*fixtureFile, error) {
