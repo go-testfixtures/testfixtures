f152f7f97aa212e3e70f8e5d2627a04759f84fcb
Andrey Nering <andrey.nering@gmail.com>
2017-06-03 15:29:11 -0300

    Move errors to its own file

--- errors.go
+++ errors.go
@@ -1,7 +1,22 @@
 package testfixtures
 
 import (
+	"errors"
 	"fmt"
+)
+
+var (
+	// ErrWrongCastNotAMap is returned when a map is not a map[interface{}]interface{}
+	ErrWrongCastNotAMap = errors.New("Could not cast record: not a map[interface{}]interface{}")
+
+	// ErrFileIsNotSliceOrMap is returned the the fixture file is not a slice or map.
+	ErrFileIsNotSliceOrMap = errors.New("The fixture file is not a slice or map")
+
+	// ErrKeyIsNotString is returned when a record is not of type string
+	ErrKeyIsNotString = errors.New("Record map key is not string")
+
+	// ErrNotTestDatabase is returned when the database name doesn't contains "test"
+	ErrNotTestDatabase = errors.New(`Loading aborted because the database name does not contains "test"`)
 )
 
 // InsertError will be returned if any error happens on database while
--- testfixtures.go
+++ testfixtures.go
@@ -2,7 +2,6 @@ package testfixtures
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"path"
@@ -33,18 +32,6 @@ type insertSQL struct {
 }
 
 var (
-	// ErrWrongCastNotAMap is returned when a map is not a map[interface{}]interface{}
-	ErrWrongCastNotAMap = errors.New("Could not cast record: not a map[interface{}]interface{}")
-
-	// ErrFileIsNotSliceOrMap is returned the the fixture file is not a slice or map.
-	ErrFileIsNotSliceOrMap = errors.New("The fixture file is not a slice or map")
-
-	// ErrKeyIsNotString is returned when a record is not of type string
-	ErrKeyIsNotString = errors.New("Record map key is not string")
-
-	// ErrNotTestDatabase is returned when the database name doesn't contains "test"
-	ErrNotTestDatabase = errors.New(`Loading aborted because the database name does not contains "test"`)
-
 	dbnameRegexp = regexp.MustCompile("(?i)test")
 )
 
