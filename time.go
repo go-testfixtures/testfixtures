3babf888fe1fd370b1cece6f6127b51c4ec06086
Garrett Massman <gmassman20@gmail.com>
2019-12-16 10:48:55 -0800

    Support more timezones formats, default to UTC

--- time.go
+++ time.go
@@ -15,8 +15,16 @@ var timeFormats = []string{
 	"02/01/2006",
 	"02/01/2006 15:04",
 	"02/01/2006 15:04:05",
+	// time zone formats
 	"2006-01-02T15:04-07:00",
 	"2006-01-02T15:04:05-07:00",
+	"2006-01-02T15:04:05Z07:00",
+	"2006-01-02 15:04:05Z07:00",
+	"2006-01-02T15:04:05Z0700",
+	"2006-01-02 15:04:05Z0700",
+	"2006-01-02T15:04:05Z07",
+	"2006-01-02 15:04:05Z07",
+	"2006-01-02 15:04:05 MST",
 }
 
 // ErrCouldNotConvertToTime is returns when a string is not a reconizable time format
@@ -24,7 +32,7 @@ var ErrCouldNotConvertToTime = errors.New("Could not convert string to time")
 
 func tryStrToDate(s string) (time.Time, error) {
 	for _, f := range timeFormats {
-		t, err := time.ParseInLocation(f, s, time.Local)
+		t, err := time.Parse(f, s)
 		if err != nil {
 			continue
 		}
