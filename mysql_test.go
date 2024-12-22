aa7adeefb9efe0ade292624e55331a6dbde14525
Krystian Chmura <krystian.chmura@gmail.com>
2024-12-22 22:10:52 +0100

    feat(mysql): make multistatements parameter optional
    The #208 PR introduced an optimisation, which requires the
    `multiStatements=true` parameter in connection string to work properly
    
    It looks like that the requirement for that parameter (which is stated
    in `README.md`) was not honored by users (e.g #240), so I want to
    keep the old behavior for convenience
    
    This commit:
    * remove mention about parameter requirement from `README.md`
    * revert default to `one query per one statement`
    * allows to keep `one query per multiple statements` logic with a
      `testfixtures.AllowMultipleStatementsInOneQuery` flag
    * some tests for both use cases

--- mysql_test.go
+++ mysql_test.go
@@ -11,6 +11,12 @@ import (
 
 func TestMySQL(t *testing.T) {
 	db := openDB(t, "mysql", os.Getenv("MYSQL_CONN_STRING"))
-	loadSchemaInOneQuery(t, db, "testdata/schema/mysql.sql")
+	loadSchemaInBatchesBySplitter(t, db, "testdata/schema/mysql.sql", []byte(";\n"))
 	testLoader(t, db, "mysql")
+}
+
+func TestMySQLWithMultipleStatementsSupport(t *testing.T) {
+	db := openDB(t, "mysql", os.Getenv("MYSQL_CONN_STRING")+"?multiStatements=true")
+	loadSchemaInOneQuery(t, db, "testdata/schema/mysql.sql")
+	testLoader(t, db, "mysql", AllowMultipleStatementsInOneQuery())
 }
